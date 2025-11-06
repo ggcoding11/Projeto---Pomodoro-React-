13a64ddd1c017bf619bdd526bd42565c05f6ffe5
Gustavo <gustavogomes1108@gmail.com>
2025-11-06 17:31:04 -0300

    .

--- Projeto/pomodoro-project/src/App.jsx
+++ Projeto/pomodoro-project/src/App.jsx
@@ -1,24 +1,40 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState } from "react";
+import reactLogo from "./assets/react.svg";
+import viteLogo from "/vite.svg";
+import "./App.css";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [count, setCount] = useState(0);
 
   return (
-    <>
-      <div>
-        <div className='container d-flex'>
-          <div>oi</div>
-          <div>ola</div>
+    <div className="container">
+      <div className="row">
+        <div className="col-12 text-center">
+          <h1>Pomodoro Timer</h1>
         </div>
-
-
-
       </div>
-    </>
-  )
+      <div className="row">
+        <div className="col-12 d-flex justify-content-center gap-2">
+          <button>Pomodoro</button>
+          <button>Short Break</button>
+          <button>Long Break</button>
+        </div>
+      </div>
+      <div className="row">
+        <div className="col-12 d-flex justify-content-center">
+          <div className="container-timer">
+            <span className="timer">25:00</span>
+          </div>
+        </div>
+      </div>
+      <div className="row">
+        <div className="col-12 d-flex justify-content-center gap-2">
+          <button>START</button>
+          <button>STOP</button>
+        </div>
+      </div>
+    </div>
+  );
 }
 
-export default App
+export default App;
