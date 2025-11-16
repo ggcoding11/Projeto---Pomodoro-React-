6cf542b2bd3813c9b4267856d89fbe662497c525
Gustavo <gustavogomes1108@gmail.com>
2025-11-15 23:17:55 -0300

    Insercao dos useState

--- Projeto/pomodoro-project/src/App.jsx
+++ Projeto/pomodoro-project/src/App.jsx
@@ -1,13 +1,33 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import reactLogo from "./assets/react.svg";
 import viteLogo from "/vite.svg";
 import "./App.css";
 
 function App() {
-  const [count, setCount] = useState(0);
+  //const [count, setCount] = useState(0);
+
+  const [estaLigado, setEstaLigado] = useState(false);
+  const [stateBotaoStartStop, setStateBotaoStartStop] = useState("START");
+  const [tempoRestante, setTempoRestante] = useState(25 * 60);
+
+  const clicarBotaoStartStop = () => {
+    setEstaLigado(!estaLigado);
+  };
+
+  useEffect(() => {
+    if (estaLigado == true) {
+      setStateBotaoStartStop("STOP");
+
+      setInterval(() => {
+        setTempoRestante((tempoRestante) => tempoRestante - 1);
+      }, 1000);
+    } else {
+      setStateBotaoStartStop("START");
+    }
+  }, [estaLigado]);
 
   return (
-    <div className="container-fluid vh-100 main">
+    <div className="container-fluid vh-100 py-4 main">
       <div className="row">
         <div className="col-12 text-center">
           <h1 className="text-white fw-bold">
@@ -16,7 +36,7 @@ function App() {
           </h1>
         </div>
       </div>
-      <div className="row row-buttons-timers mt-2">
+      <div className="row row-buttons mt-2">
         <div className="col-12 d-flex justify-content-center gap-2">
           <button className="btn text-white fw-semibold">Pomodoro</button>
           <button className="btn text-white fw-semibold">Short Break</button>
@@ -26,14 +46,21 @@ function App() {
       <div className="row mt-4">
         <div className="col-12 d-flex justify-content-center">
           <div className="container-timer d-flex justify-content-center align-items-center">
-            <span className="timer text-center text-white fw-bold">25:00</span>
+            <span className="text-center text-white fw-bold" id="timer">
+              {tempoRestante}
+            </span>
           </div>
         </div>
       </div>
-      <div className="row mt-4">
+      <div className="row row-buttons mt-4">
         <div className="col-12 d-flex justify-content-center gap-2">
-          <button>START</button>
-          <button>STOP</button>
+          <button
+            onClick={clicarBotaoStartStop}
+            className="btn text-white fw-semibold"
+            id="start-stop"
+          >
+            {stateBotaoStartStop}
+          </button>
         </div>
       </div>
     </div>
